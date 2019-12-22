4f31cf95a7d0678431cb7dbb11bd49375600949f
Felipe <felipeagc2@gmail.com>
2019-12-22 19:09:44 -0300

    Better interface usage

--- examples/simple.c
+++ examples/simple.c
@@ -9,22 +9,26 @@ int main(int argc, char *argv[]) {
   MtArena arena;
   mt_arena_init(&arena, 1 << 14);
 
-  MtVulkanWindowSystem *window_system = mt_glfw_vulkan_init();
+  MtIWindowSystem window_system;
+  mt_glfw_vulkan_init(&window_system);
 
   MtIDevice device;
   mt_vulkan_device_init(
       &device,
-      &(MtVulkanDeviceDescriptor){.window_system = window_system},
+      &(MtVulkanDeviceDescriptor){.window_system = &window_system},
       &arena);
 
   MtIWindow window;
   mt_glfw_vulkan_window_init(&window, 800, 600, "Hello", &arena);
 
+  while (!window.vt->should_close(window.inst)) {
+    window_system.vt->poll_events();
+  }
+
   window.vt->destroy(window.inst);
-
   device.vt->destroy(device.inst);
+  window_system.vt->destroy();
 
-  mt_glfw_vulkan_destroy();
   mt_arena_destroy(&arena);
   return 0;
 }
--- include/motor/window.h
+++ include/motor/window.h
@@ -2,10 +2,23 @@
 #define MT_WINDOW_H
 
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef struct MtWindow MtWindow;
+typedef struct MtWindowSystem MtWindowSystem;
+
+typedef struct MtWindowSystemVT {
+  void (*poll_events)(void);
+  void (*destroy)(void);
+} MtWindowSystemVT;
+
+typedef struct MtIWindowSystem {
+  MtWindowSystem *inst;
+  MtWindowSystemVT *vt;
+} MtIWindowSystem;
 
 typedef struct MtWindowVT {
+  bool (*should_close)(MtWindow *);
   void (*destroy)(MtWindow *);
 } MtWindowVT;
 
